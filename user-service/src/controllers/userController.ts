a5e03af9cac5466163bd0354b2483b3cc87afb5b
Derekqua <derekqua8@gmail.com>
2026-03-17 22:54:52 +0800

    Store avatar options in cache

--- user-service/src/controllers/userController.ts
+++ user-service/src/controllers/userController.ts
@@ -137,6 +137,7 @@ export const updateProfileIcon = async (req: Request, res: Response) => {
 };
 
 export const getAvatarOptions = (_req: Request, res: Response) => {
+  res.set('Cache-Control', 'public, max-age=21600, immutable');
   const avatars = AVATAR_OPTIONS.map((key) => ({
     key,
     url: `${process.env.GCS_BUCKET_URL}/${key}.png`,
