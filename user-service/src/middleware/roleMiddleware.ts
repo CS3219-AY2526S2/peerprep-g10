ebf4e32c8bed6078443d0155c43181ae1c35ef78
Derekqua <derekqua8@gmail.com>
2026-03-17 22:11:46 +0800

    Fix update profile bug

--- user-service/src/middleware/roleMiddleware.ts
+++ user-service/src/middleware/roleMiddleware.ts
@@ -4,7 +4,7 @@ export const authorizeRoles = (...allowedRoles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const user = (req as any).user; // This data was attached by authenticateToken
 
-    if (!user || !allowedRoles.includes(user.role)) {
+    if (!user || !allowedRoles.includes(user.access_role)) {
       return res.status(403).json({ 
         message: `Access Denied: You do not have the required permissions (${allowedRoles.join(', ')})` 
       });
