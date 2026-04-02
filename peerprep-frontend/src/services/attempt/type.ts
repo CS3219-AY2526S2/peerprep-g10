277907173e23892e9b0a415b92848b9559be3377
Derekqua <derekqua8@gmail.com>
2026-04-02 12:25:40 +0800

    Add frontend attemptAPI

---
+++ peerprep-frontend/src/services/attempt/type.ts
@@ -0,0 +1,11 @@
+export type Attempt = {
+  id: string;
+  roomId: string;
+  userId: string;
+  partnerId: string;
+  questionId: string;
+  code: string;
+  startedAt: string;
+  endedAt: string;
+  createdAt: string;
+};
